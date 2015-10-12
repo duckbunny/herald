faba3031c061a0a0df4eb89d82c3b73dcb9ce6a6
Jason Smith <jasonrichardsmith@gmail.com>
2015-10-12 08:56:51 -0400

    added baseline for herald package

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Copyright © 2015 Jason Smith <jasonrichardsmith@gmail.com>.
+//
+// Use of this source code is governed by the LGPL-3
+// license that can be found in the LICENSE file.
+package herald
