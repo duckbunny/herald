28ec1eb222916df42937c677bb057c765779d2ca
Jason Smith <jasonrichardsmith@gmail.com>
2015-10-21 08:28:45 -0400

    added more docs, dropped new, added multiple add pool and declare

--- doc.go
+++ doc.go
@@ -2,4 +2,28 @@
 //
 // Use of this source code is governed by the LGPL-3
 // license that can be found in the LICENSE file.
+
+/*
+The herald package is intended to provide the tools to make your
+microservice public to other declared microservices.
+
+This tool facilitates two functions.
+
+Pool
+
+The pool interface allows the microservice to support many API routers through
+a pool registry.
+
+Declaration
+
+The declaration interface allows the microservice to declare itself in a registry
+of services, so other services can consume that configuration and automate
+common actions.  By utilizing a Declaration registry a microservice can support
+many systems.
+
+
+A package can support one or both of the interfaces, and the interface to be
+used will be determined by environment variables or flags.
+
+*/
 package herald
--- herald.go
+++ herald.go
@@ -11,11 +11,15 @@ import (
 	"os"
 )
 
+// PoolTypes are a collection of Pool Interfaces identified by strings
+// associated with a pooling mechanism.
 var PoolTypes map[string]Pool = make(map[string]Pool)
 
+// DeclarationTypes are a collection of Declaration Interfaces identified by
+// strings associated with a declaration mechanism.
 var DeclarationTypes map[string]Declaration = make(map[string]Declaration)
 
-// Flags
+// Flags to be parsed for setting pool and declaration interfaces.
 var heraldPool string
 var heraldDeclare string
 var heraldBoth string
@@ -26,12 +30,15 @@ func init() {
 	flag.StringVar(&heraldBoth, "herald", "", "Herald to handle both declaration and pooling.")
 }
 
+// Herald is a wrapper structure of the pooling and declaration interfaces that will be implemented
+// based on flags or environement variables.
 type Herald struct {
 	Pool
 	Declaration
 	Service *service.Service
 }
 
+// Set declared pool and declaration for this instance.
 func setFlagEnv() {
 	if !flag.Parsed() {
 		flag.Parse()
@@ -55,18 +62,23 @@ func setFlagEnv() {
 	return
 }
 
-func New(s *service.Service) *Herald {
-	return &Herald{Service: s}
-}
-
+// Helper function to build herald for the currently running microservice.
 func This() (*Herald, error) {
 	setFlagEnv()
 	h := &Herald{}
-	if pt, ok := PoolTypes[heraldPool]; ok {
-		h.Pool = pt
+	if heraldPool != "" {
+		if pt, ok := PoolTypes[heraldPool]; !ok {
+			err := fmt.Errorf("Attempt to utilize unrecognized Pooling mechanism %v", heraldPool)
+			return h, err
+		}
+		h.Pool = PoolTypes[heraldPool]
 	}
-	if dt, ok := DeclarationTypes[heraldDeclare]; ok {
-		h.Declaration = dt
+	if heraldDeclare != "" {
+		if dt, ok := DeclarationTypes[heraldDeclare]; !ok {
+			err := fmt.Errorf("Attempt to utilize unrecognized Declaration mechanism %v", heraldPool)
+			return h, err
+		}
+		h.Declaration = DeclarationTypes[heraldDeclare]
 	}
 	s, err := service.This()
 	h.Service = s
@@ -86,33 +98,62 @@ func (h *Herald) Init() error {
 	return nil
 }
 
+// Add this service to pool of microservices.
 func (h *Herald) StartPool() error {
 	return h.Pool.Start(h.Service)
 }
 
+// Remove this service from pool of microservices.
 func (h *Herald) StopPool() error {
 	return h.Pool.Stop(h.Service)
 }
 
+// Declare this microservice definition.
 func (h *Herald) Declare() error {
 	return h.Declaration.Declare(h.Service)
 }
 
+// Get a foreign microservice definition.
+func (h *Herald) Get(s *service.Service) error {
+	return h.Declaration.Get(s)
+}
+
+// Pool defines an interface that will add and remove a microservice
+// to a pool of microservices.
 type Pool interface {
 	Start(*service.Service) error
 	Stop(*service.Service) error
 	Init() error
 }
 
-type Declaration interface {
-	Declare(*service.Service) error
-	Init() error
-}
-
+// Add a single pool.
 func AddPool(key string, p Pool) {
 	PoolTypes[key] = p
 }
 
+// Add Multiple pools at once.
+func AddPools(ps map[string]Pool) {
+	for key, pool := range ps {
+		PoolTypes[key] = pool
+	}
+}
+
+// Declaration defines an interface that will broadcast the microservice
+// definition, for other services to digest.
+type Declaration interface {
+	Declare(*service.Service) error
+	Get(*service.Service) error
+	Init() error
+}
+
+// Add a single declaration.
 func AddDeclaration(key string, d Declaration) {
 	DeclarationTypes[key] = d
+}
+
+// Add Multiple declarations at once.
+func AddDeclarations(ds map[string]Declaration) {
+	for key, dec := range ds {
+		DeclarationTypes[key] = d
+	}
 }
